1343d8ce5bfca4da69c51fef6d20fb9998030bcd
Monika Jozefina <kozefina@MacBook-Air-Monika.local>
2020-06-02 10:26:46 +0200

    First Commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Calculator {
+    public static void main(String args[]) {
+        int x = 10;
+        int y = 8;
+        int sum = x + y;
+        int result = x - y;
+        System.out.println(sum);
+        System.out.println(result);
+    }
 }
